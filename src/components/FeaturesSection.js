2ba7e71978896977c3b38f0ce1779424dbfdf318
Stackbit Code Editor <projects@stackbit.com>
2021-07-30 10:15:26 +0000

    FeaturesSection.js: changed

--- src/components/FeaturesSection.js
+++ src/components/FeaturesSection.js
@@ -43,6 +43,7 @@ export default class FeaturesSection extends React.Component {
                         </div>
                     )}
                 </div>
+                Ty
             </div>
         )
     }
